383362c91f58faf54152d427342df27669fe2bf4
Felix Yuan <Felix@felixs-air.hsd1.ca.comcast.net>
2014-08-03 13:14:53 -0700

    Added day 7 exercises

---
+++ Day 7/exercises.js
@@ -0,0 +1,88 @@
+/* This will be a review of different objects we learned about in class.
+ * First up Strings
+ * Second is Arrays
+ * Third are objects
+ */
+
+// String exercise 1
+// A palindrome is a string that reads forwards and backwards the same.
+// Cat is not a palindrome
+// Ana is a palindrome
+// racecar is a palindrome
+// A man a plan a canal is a palindrome.
+// Write a method that takes a string and determines whether or not it's a palindrome
+//
+//
+// String exercise 2
+// Write a function that takes a string and reverses it. You are not allowed to use string reverse.
+//
+//
+// String exercise 3
+// Write a function that takes a string and and a number and rotates it right.
+// For example of I took cat and rotated it 1 then I would get tca. If I rotated it 2 I'd get atc.
+//
+//
+// String exercise 4
+// Write a function that takes two strings. Then figure out whether or not string 2 is a part of string 1.
+// String 2 is a part of string 1 if the entire string is found in a single contiguous block.
+// for example: race is in racecar, car is in racecar
+//
+//
+// Array exercise 1
+// Write a function that takes an array and another variable.
+// If the element exists in the array, return its index. Else return -1
+//
+//
+// Array exercise 2
+// Write a function that takes an array. Reverse the array.
+// BONUS: can you do this in place?
+//
+//
+// Array exercise 3
+// Write a function that takes an array and a variable. Add the variable to the end of the array.
+// Hint: There's a builtin to do this.
+//
+//
+// Array exercise 4
+// Write a function that returns the last element of the array and removes it from the array.
+// Hint: there's a built in to do this.
+//
+//
+// Array and string exercise 5
+// Write a function that takes a string. Split the string on every comma and add it to the list.
+//
+//
+// Array and String exercise 6
+// Write a function that takes an array. Join all the elements of the array together into a string, separated by commas
+//
+//
+// Array exercise 7
+// Write a function that takes two arrays.
+// Then find the intersection between the two arrays.
+// The intersection of two sets is defined as this:
+// if you have set 1 with [1, 2, 3]
+// and you have set two with [2, 3, 10]
+// then the intersection is [2, 3]
+// BONUS: Assume the two arrays are sorted. Does this change how you can do this problem?
+// Part 2: write a union function
+// A union between two sets.
+// If you have set 1 [1, 2, 3] and set 2 [2, 3, 10]
+// the union of the two sets is [1, 2, 3, 10]
+//
+//
+// Object exercise 1
+// Write a function that takes an object. Now return an array of all the keys in that array
+//
+//
+// Object exercise 2
+// Redo array exercise 7. This time inside the function use an object to help you with the intersection.
+//
+//
+// Object exercise 3
+// Write a function that takes an array of objects. Each object will have the following keys:
+// {
+//  title: 'foobar',
+//  author: 'Toonces',
+//  readingStatus: false
+// }
+// Now print out the title of every book along with its reading status

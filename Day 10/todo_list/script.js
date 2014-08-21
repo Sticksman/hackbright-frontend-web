6acc9c350d850c191d3c7cafa5bc721bc6a63b73
Felix Yuan <felix@Felixs-MacBook-Air-2.local>
2014-08-20 20:04:29 -0700

    Commit changes to code for assignment

--- Day 10/todo_list/script.js
+++ Day 10/todo_list/script.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
-    var addItemBtn = $('#add-item-btn'),
+
+    var COMPLETE_EVENT_STR = 'COMPLETE-ITEM',
+        addItemBtn = $('#add-item-btn'),
         addItemBox = $('#add-item-box'),
+        removeItemBtn = $('#remove-item-btn'),
+        completeItemBtn = $('#complete-item-btn'),
         todoListWrapper = $('.todo-list-wrapper'),
+        doneListWrapper = $('.done-list-wrapper'),
         addItemHandler = function(name) {
             var todoListItem = $('<li/>').addClass('todo-item'),
                 checkbox = $('<input />').attr('type', 'checkbox');
@@ -14,9 +19,17 @@ $(document).ready(function(){
             todoListItem.append(checkbox).append(name);
             todoListWrapper.append(todoListItem);
             addItemBox.val('');
+            todoListItem.on(COMPLETE_EVENT_STR, function() {
+                var _this = $(this),
+                    fn = function() {
+                        _this.remove()
+                    };
+                fn();
+                doneListWrapper.append(_this);
+
+            });
             return false;
         };
-
 
     addItemBtn.on('click', function() {
         // "  foo  ".trim() = "foo"
@@ -31,6 +44,29 @@ $(document).ready(function(){
             addItemHandler(name);
         }
     });
+
+    removeItemBtn.on('click', function() {
+        var checkedItems = $('.todo-item').children('input[type=checkbox]:checked');
+
+        checkedItems.parents('.todo-item').remove();
+    });
+    completeItemBtn.on('click', function() {
+        var checkedItems = $('.todo-item').children('input[type=checkbox]:checked'),
+            todoListItems = checkedItems.parents('.todo-item');
+
+        todoListItems.addClass('complete');
+        todoListItems.trigger(COMPLETE_EVENT_STR);
+    });
+
+    /*
+     * $.ajax({
+     *      url: 'items.json',
+     *      success: function(response) {
+     *          // Same stuff in here.
+     *      }
+     * });
+     *
+     */
     $.getJSON('items.json', function(response) {
         var listItems = response.listItems;
         $.each(response.listItems, function(i, el) {
